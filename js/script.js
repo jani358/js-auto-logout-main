ed2d57628c33708e5e9349d1056a729d3f1b2b12
Janindu Gayanga <janindugayanga10@gmail.com>
2023-01-18 09:23:15 +0530

    change

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//Logout takes 5 seconds. If the mouse moves on the main page before the 5 seconds expires, the reset time is set.Then a countdown starts from the beginning.
+
+var timer = document.getElementById("timer");
+var duration = 5; // duration in seconds
+
+setInterval(updateTimer, 1000);
+
+function updateTimer() {
+	duration--;
+	if ( duration < 1 ) {
+		window.location = "logout.html";
+	} else {
+		timer.innerText = "Auto Logout In: " + formatTime(duration);
+	}	
+}
+
+window.addEventListener("mousemove", resetTimer);
+
+function resetTimer() {
+	duration = 15;
+}
+
+function formatTime(timeInSeconds) {
+	var minutes = Math.floor(timeInSeconds / 60);
+	var seconds = timeInSeconds % 60;
+	if ( minutes < 10 ) { minutes = "0" + minutes; }
+	if ( seconds < 10 ) { seconds = "0" + seconds; }
+	return minutes + ":" + seconds;
+}
